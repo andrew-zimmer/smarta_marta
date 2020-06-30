2ed51280e214cc640d72eaf8b25b46441be68d2f
andrew-zimmer <andrewzimmer.dev@gmail.com>
2020-06-30 14:40:19 -0400

    heroku setup

--- Gemfile
+++ Gemfile
@@ -5,15 +5,22 @@ gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3', '~> 1.3.6'
 gem 'thin'
-gem 'shotgun'
-gem 'pry'
 gem 'bcrypt'
-gem 'tux'
 gem 'dotenv'
 gem 'sinatra-flash'
+gem 'foreman'
 
+group :development do
+  gem 'sqlite3', '~> 1.3.6'
+  gem 'tux'
+  gem 'shotgun'
+  gem 'pry'
+end
+
+group :production do
+  gem 'pg', '~>0.20'
+end
 
 
 group :test do
--- config/environment.rb
+++ config/environment.rb
@@ -11,14 +11,16 @@ require 'sinatra/flash'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require 'dotenv/load'
+require 'dotenv/load' if ENV['SINATRA_ENV'] == 'development'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 ActiveRecord::Base.time_zone_aware_attributes = [:datetime]
+
+set :database_file, "./database.yml"
 
 require './app/controllers/application_controller'
 require_all 'app'
